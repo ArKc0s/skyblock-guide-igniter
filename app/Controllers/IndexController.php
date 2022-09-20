1b2034cb5537e2982756f31d63e67c8c323d91e3
Léo Wadin <46723431+ArKc0s@users.noreply.github.com>
2022-09-20 17:28:05 +0200

    removed errors printing

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -13,9 +13,6 @@ class IndexController extends BaseController {
 
         $playerName = $this->request->getVar('playerName');
         print_r($playerName);
-        ini_set('display_errors', '1');
-        ini_set('display_startup_errors', '1');
-        error_reporting(E_ALL);
 
         return redirect()->to('/player/'.$playerName);
 
